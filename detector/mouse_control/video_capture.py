a47e7082fe0d2dee66cb86bd2385991c37fc8d79
chrisgal77 <krzysztof.galus.k7@gmail.com>
2021-09-16 10:34:05 +0200

    removed unneeded files

--- detector/mouse_control/video_capture.py
+++ detector/mouse_control/video_capture.py
@@ -3,12 +3,12 @@ import cv2
 from exceptions import FrameReadError
 
 
-class VideoCapture:
-    def __init__(self):
-        self.capture = cv2.VideoCapture(0)
+class VideoCapture(cv2.VideoCapture):
+    def __init__(self, *args, **kwargs):
+        super(VideoCapture, self).__init__(*args, **kwargs)
 
     def __call__(self, *args, **kwargs):
-        success, image = self.capture.read()
+        success, image = self.read()
 
         if not success:
             raise FrameReadError('Failed to read a frame')
